0e7299c7a94849f56109f4b74fb8cce2a503f944
David Schnurr <david.schnurr@gmx.de>
2025-12-03 12:31:09 +0100

    Minor config change

--- PFNs/configs/tabpfn_prior_config_very_large_2_gpu.py
+++ PFNs/configs/tabpfn_prior_config_very_large_2_gpu.py
@@ -57,7 +57,7 @@ def get_config(config_index: int = 0) -> MainConfig:
         min_single_eval_pos=24,
         max_seq_len=1024,
         min_num_features=2,
-        max_num_features=50,
+        max_num_features=25,
         fixed_num_test_instances=None,
     )
 
