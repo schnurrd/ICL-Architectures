a9f0b79ee8ac747d2505599d2690557b8121067c
David Schnurr <david.schnurr@gmx.de>
2025-12-20 22:22:58 +0100

    Minor config change

--- PFNs/configs/tabpfn_prior_config_1_gpu_v4_group_3.py
+++ PFNs/configs/tabpfn_prior_config_1_gpu_v4_group_3.py
@@ -39,7 +39,7 @@ def get_config(config_index: int = 0) -> MainConfig:
     )
     
     batch_shape = BatchShapeSamplerConfig(
-        batch_size=8,
+        batch_size=4,
         min_single_eval_pos=24,
         max_seq_len=1000,
         min_num_features=2,
@@ -91,12 +91,12 @@ def get_config(config_index: int = 0) -> MainConfig:
         batch_shape_sampler=batch_shape,
         epochs=200,
         warmup_epochs=10,
-        steps_per_epoch=250,
+        steps_per_epoch=500,
         n_targets_per_input=1,
         train_mixed_precision=True,
         scheduler="cosine_decay",
         progress_bar=True,
         wandb=wandb_config,
         num_workers=4,
-        aggregate_k_gradients=1,
+        aggregate_k_gradients=2,
     )
